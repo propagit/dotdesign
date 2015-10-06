9a1d94f38a1bf6c098328502970216989c619916
Carl Worricker <carlworricker@Carls-MBP.gateway>
2015-10-06 14:23:12 +1100

    ammedments

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -12,7 +12,9 @@
                     <div class="carousel-caption">
                         <h1>15 years & counting</h1>
                      	<h4>over 15 years delivering product innovation in on & off premise</h4>
-                        <button class="btn btn-app">read more</button>
+                        <a href="http://www.propatest.com/dotdesign/about">
+                            <button class="btn btn-app">read more</button>
+                        </a>
                     </div>
                 </div>
             </div>
